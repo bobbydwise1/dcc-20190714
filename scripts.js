5bae92ad2075d40d076e5807f10930ecd4e1793d
Robert Lee <bobbydwise1@hotmail.com>
2019-07-14 14:39:48 -0700

    Add functions.

--- scripts.js
+++ scripts.js
@@ -8,8 +8,41 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120
 Follow-up: what if you can't use division?
 */
 
-// $(document).ready(function() {
-//   $('#output-section-1').text(myList);
-//   $('#output-section-2').text(tops);
-//   $('#output-section-3').text(tops[0]+tops[1]);
-//   });
+const makeRandomNumOne2Ten = () => {
+  return Math.ceil(Math.random()*10);
+}
+
+const randomArray = (yourArrayLength) => {
+  var outputArray = [];
+  for (i=0; i<yourArrayLength; i++) {
+    outputArray.push(makeRandomNumOne2Ten());
+  }
+  return outputArray;
+}
+
+startArray = randomArray(makeRandomNumOne2Ten());
+console.log('startArray = ';startArray);
+
+const productOfNonIndex = (yourArray) => {
+  var outputArray = [];
+  for (i=0; i<yourArray.length; i++) {
+    var tempIndexProduct = 1;
+    for (j=0; j<yourArray.length; j++) {
+      if ( j === i ) {
+        continue;
+      }
+      tempIndexProduct = tempIndexProduct*yourArray[i];
+    }
+    outputArray.push(tempIndexProduct);
+    }
+  return outputArray;
+}
+
+answerArray = productOfNonIndex(startArray);
+console.log('answerArray =', answerArray;);
+
+$(document).ready(function() {
+  $('#output-section-1').text(startArray);
+  $('#output-section-2').text(answerArray);
+  //$('#output-section-3').text(tops[0]+tops[1]);
+  });
